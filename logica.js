3c018eb1336c0b721718a43c665aad9a6a733bc4
JESGOD <yonnyes@unicauca.edu.co>
2023-04-15 10:58:29 -0500

    se agrego decoracion a el btn copiar

--- logica.js
+++ logica.js
@@ -2,64 +2,82 @@ let texto_usuario = document.querySelector(".input-texto");
 let contenedor_resultado = document.querySelector(".resultado");
 let contenedor_sinResultado = document.querySelector(".sin_resultado");
 let btns = document.querySelectorAll(".btn");
-let textpProcesado = document.querySelector(".resultado")
+let textpProcesado = document.querySelector(".resultado");
 
 let array = [];
 let array2 = [];
+let acentos = /[áéíóú]/;
+let caracteresEspeciales = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
 
 /* ENCRIPTAR */
 btns[0].addEventListener("click", () => {
-  /* comprobamos si el texto contiene alguna de las vocales */
-  if (/[aeiou]/gi.test(texto_usuario.value)) {
-    contenedor_sinResultado.classList = "sin_resultado esconder";
-    contenedor_resultado.classList.add("aparecer")
-    /* quitamos espacios al inicio y al final de la cadena y guardamos en un arreglo el texto separado por letras */
-    array = texto_usuario.value.toLowerCase().trim().split("");
-    /* si el arrglo(el texto) contiene alguna de las vocales se hace el cambio de dicha vocal por su respectivo valor nuevo  */
-    let i = 0;
-    while (i < array.length) {
-      if (array[i] === "a") array[i] = "ai";
+  if (
+    acentos.test(texto_usuario.value) ||
+    caracteresEspeciales.test(texto_usuario.value)
+  ) {
+    texto_usuario.innerHTML = "";
+    alert("el texto ingresado no puede tener caracteres especiales ni acentos");
+    
+  } else {
+    /* comprobamos si el texto contiene alguna de las vocales */
+    if (/[aeiou]/gi.test(texto_usuario.value)) {
+      contenedor_sinResultado.classList = "sin_resultado esconder";
+      contenedor_resultado.classList.add("aparecer");
+      /* quitamos espacios al inicio y al final de la cadena y guardamos en un arreglo el texto separado por letras */
+      array = texto_usuario.value.toLowerCase().trim().split("");
+      /* si el arrglo(el texto) contiene alguna de las vocales se hace el cambio de dicha vocal por su respectivo valor nuevo  */
+      let i = 0;
+      while (i < array.length) {
+        if (array[i] === "a") array[i] = "ai";
 
-      if (array[i] === "e") array[i] = "enter";
+        if (array[i] === "e") array[i] = "enter";
 
-      if (array[i] === "i") array[i] = "imes";
+        if (array[i] === "i") array[i] = "imes";
 
-      if (array[i] === "o") array[i] = "ober";
+        if (array[i] === "o") array[i] = "ober";
 
-      if (array[i] === "u") array[i] = "ufat";
+        if (array[i] === "u") array[i] = "ufat";
 
-      i++;
+        i++;
+      }
+      /* se une el texto y se lo pasamos a el contenedor que mostrara el resultado al ususario por pantalla */
+      contenedor_resultado.innerHTML = array.join("");
     }
-    /* se une el texto y se lo pasamos a el contenedor que mostrara el resultado al ususario por pantalla */
-    contenedor_resultado.innerHTML = array.join("");
   }
 });
 /*FIN ENCRIPTAR */
 
 /* DESENCRIPTAR */
 btns[1].addEventListener("click", () => {
+  if (
+    acentos.test(texto_usuario.value) ||
+    caracteresEspeciales.test(texto_usuario.value)
+  ) {
+    alert("el texto ingresado no puede tener caracteres especiales ni acentos");
+    texto_usuario.innerHTML = "";
+  } else {
+    if (/(ai|enter|imes|ober|ufat)/gi.test(texto_usuario.value)) {
+      contenedor_sinResultado.classList = "sin_resultado esconder";
+      contenedor_resultado.classList.add("aparecer");
+      array2 = texto_usuario.value.toLowerCase().trim().split(" ");
 
-  if (/(ai|enter|imes|ober|ufat)/gi.test(texto_usuario.value)) {
-    contenedor_sinResultado.classList = "sin_resultado esconder";
-    contenedor_resultado.classList.add("aparecer")
-    array2 = texto_usuario.value.toLowerCase().trim().split(" ");
-
-    let j = 0;
-    while (j < array2.length) {
-      if (array2[j].includes("ai"))
-        array2[j] = array2[j].replace(/(ai)/gi, "a");
-      if (array2[j].includes("enter"))
-        array2[j] = array2[j].replace(/(enter)/gi, "e");
-      if (array2[j].includes("imes"))
-        array2[j] = array2[j].replace(/(imes)/gi, "i");
-      if (array2[j].includes("ober"))
-        array2[j] = array2[j].replace(/(ober)/gi, "o");
-      if (array2[j].includes("ufat"))
-        array2[j] = array2[j].replace(/(ufat)/gi, "u");
-      j++;
+      let j = 0;
+      while (j < array2.length) {
+        if (array2[j].includes("ai"))
+          array2[j] = array2[j].replace(/(ai)/gi, "a");
+        if (array2[j].includes("enter"))
+          array2[j] = array2[j].replace(/(enter)/gi, "e");
+        if (array2[j].includes("imes"))
+          array2[j] = array2[j].replace(/(imes)/gi, "i");
+        if (array2[j].includes("ober"))
+          array2[j] = array2[j].replace(/(ober)/gi, "o");
+        if (array2[j].includes("ufat"))
+          array2[j] = array2[j].replace(/(ufat)/gi, "u");
+        j++;
+      }
+      console.log((contenedor_resultado.innerHTML = array2.join(" ")));
+      contenedor_resultado.innerHTML = array2.join(" ");
     }
-    console.log(contenedor_resultado.innerHTML = array2.join(" "));
-    contenedor_resultado.innerHTML = array2.join(" ");
   }
 });
 /* hace aparecer la imagen y el texto en la seccion de resultados(mensaje ecriptado/decem) */
@@ -67,10 +85,8 @@ if (!contenedor_resultado.value) {
   contenedor_sinResultado.classList = "sin_resultado aparecer";
 }
 
-
 /* copiar texto al porta papeles */
-function copiarTexto(){
-   
+function copiarTexto() {
   // Crea un campo de texto "oculto"
   var aux = document.createElement("input");
 
